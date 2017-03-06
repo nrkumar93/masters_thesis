2d45ec398a6d174014d4e916c6ac9a295e010cce
nrkumar93 <nrkumar93@gmail.com>
2017-03-06 10:20:02 -0500

    some mods

--- thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
+++ thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
@@ -8,15 +8,20 @@ data_size = length(robot.laser);
 Trans_wrt_origin = zeros(3, data_size-1);
 Rot_wrt_origin = zeros(3, 3, data_size-1);
 
+i=2;
+% k=i-1;
+
+p=2;
+
 % Preallocating the initial Rotation and Translation matrix to avoid check
 % inside the loop.
-range_1 = robot.laser(1).range;
-range_2 = robot.laser(2).range;
+range_1 = robot.laser(i-1).range;
+range_2 = robot.laser(i).range;
 [Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1)] = scan_matcher(range_1, range_2);
 
-k=1;
-i=2;
-while i < data_size-1
+hold on;
+
+while i <= data_size-1
     for j = i+1:data_size-1
         if ~isempty(robot.laser(j).range)
             break;
@@ -26,12 +31,20 @@ while i < data_size-1
     range_2 = robot.laser(j).range;
     [R, T] = scan_matcher(range_1, range_2);
     
-    Rot_wrt_origin(:,:,i) = Rot_wrt_origin(:,:,k) * R;
-    Trans_wrt_origin(:,i) = (Rot_wrt_origin(:,:,k) * T) + Trans_wrt_origin(:,k);
+    Rot_wrt_origin(:,:,p) = Rot_wrt_origin(:,:,p-1) * R;
+    Trans_wrt_origin(:,p) = (Rot_wrt_origin(:,:,p-1) * T) + Trans_wrt_origin(:,p-1);
     
-    k=i;
+    plot(robot.odom(i).pose(1), robot.odom(i).pose(2), '-o');
+    plot(Trans_wrt_origin(1,p), Trans_wrt_origin(2,p), 'r*');
+%     plot(T(1), T(2), 'r+');
+    drawnow;
+    
+    p=p+1;
+%     k=i;
     i=j;
 end
+
+% hold off
 
 pose_x = Trans_wrt_origin(1,:);
 pose_y = Trans_wrt_origin(2,:);
