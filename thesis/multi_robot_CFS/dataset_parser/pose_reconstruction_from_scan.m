2b8ebe35972ba5f8c0c7f75ca5ffa5f5db1b8897
nrkumar93 <nrkumar93@gmail.com>
2017-03-08 21:07:06 -0500

    comparing scan match and raw data

--- thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
+++ thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
@@ -19,10 +19,14 @@ range_1 = robot.laser(i-1).range;
 range_2 = robot.laser(i).range;
 [Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1)] = scan_matcher(range_1, range_2);
 
-hold on;
+data_distance = [];
+scan_match_distance = [];
+
+data_distance(1) = distance(robot.odom(i).pose, robot.odom(i-1).pose);
+scan_match_distance(1) = sqrt(Trans_wrt_origin(1,1)^2 + Trans_wrt_origin(2,1)^2);
 
 while i <= data_size-1
-    for j = i+1:data_size-1
+    for j = i+1:data_size
         if ~isempty(robot.laser(j).range)
             break;
         end
@@ -31,11 +35,15 @@ while i <= data_size-1
     range_2 = robot.laser(j).range;
     [R, T] = scan_matcher(range_1, range_2);
     
+    data_distance(p) = distance(robot.odom(i).pose, robot.odom(j).pose);
+    scan_match_distance(p) = sqrt(T(1)^2 + T(2)^2);
+    
+    
     Rot_wrt_origin(:,:,p) = Rot_wrt_origin(:,:,p-1) * R;
     Trans_wrt_origin(:,p) = (Rot_wrt_origin(:,:,p-1) * T) + Trans_wrt_origin(:,p-1);
     
-    plot(robot.odom(i).pose(1), robot.odom(i).pose(2), '-o');
-    plot(Trans_wrt_origin(1,p), Trans_wrt_origin(2,p), 'r*');
+%     plot(robot.odom(i).pose(1), robot.odom(i).pose(2), '-o');
+%     plot(Trans_wrt_origin(1,p), Trans_wrt_origin(2,p), 'r*');
 %     plot(T(1), T(2), 'r+');
     drawnow;
     
@@ -44,9 +52,15 @@ while i <= data_size-1
     i=j;
 end
 
+plot(data_distance);
+hold on;
+plot(scan_match_distance);
 % hold off
 
 pose_x = Trans_wrt_origin(1,:);
 pose_y = Trans_wrt_origin(2,:);
 pose_theta = acos(Rot_wrt_origin(1,1,:));
 
+function dist = distance(A, B)
+
+dist = sqrt((A(1) - B(1))^2 + (A(2) - B(2))^2);
