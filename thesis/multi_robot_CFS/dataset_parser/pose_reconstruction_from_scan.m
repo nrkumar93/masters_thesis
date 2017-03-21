5cb6b53c5e6bacd363a38d87b89cfd833f8d2de0
nrkumar93 <nrkumar93@gmail.com>
2017-03-21 00:17:09 -0400

    adding modes to pose_reconstruction_from_scan

--- thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
+++ thesis/multi_robot_CFS/dataset_parser/pose_reconstruction_from_scan.m
@@ -5,8 +5,23 @@ robot = getfield(robot, char(fieldnames(robot)));
 
 data_size = length(robot.laser);
 
+% Scan Matching Mode
+% 1. icpmatlab
+% 2. csm_icp_matlab
+% 3. csm_icp_c++
+
+mode = 3;
+
+% Loading all the odometry poses from the dataset
+all_poses = [];
+all_poses = [all_poses, robot.odom.pose];
+pose_x = all_poses(1:3:end);
+pose_y = all_poses(2:3:end);
+pose_theta = all_poses(3:3:end);
+
 Trans_wrt_origin = zeros(3, data_size-1);
 Rot_wrt_origin = zeros(3, 3, data_size-1);
+covariance = zeros(9, data_size-1);
 
 Trans_local = zeros(3, data_size-1);
 Rot_local = zeros(3, 3, data_size-1);
@@ -21,7 +36,19 @@ p=2;
 % inside the loop.
 range_1 = robot.laser(i-1).range;
 range_2 = robot.laser(i).range;
-[Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1)] = csm_scan_matcher(range_1, range_2);
+if mode == 1
+    [Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1)] = scan_matcher(range_1, range_2);
+elseif mode == 2
+    [Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1)] = csm_scan_matcher(range_1, range_2);    
+elseif mode == 3
+    [init_x, init_y, init_theta] = ...
+        odometry_difference(pose_x(i-1), pose_y(i-1), pose_theta(i-1), pose_x(i), pose_y(i), pose_theta(i));
+    
+    [Rot_wrt_origin(:,:,1), Trans_wrt_origin(:,1), covariance(:,1)] = ...
+        fast_csm_scan_matcher(robot.laser(i-1).measurement_time, range_1, ...
+        robot.laser(i).measurement_time, range_2, [init_x, init_y, init_theta]);
+end
+
 Trans_local(:,1) = Trans_wrt_origin(:,1);
 Rot_local(:,:,1) = Rot_wrt_origin(:,:,1);
 
@@ -39,7 +66,18 @@ while i <= data_size-offset
     end
     range_1 = robot.laser(i).range;
     range_2 = robot.laser(j).range;
-    [R, T] = csm_scan_matcher(range_1, range_2);
+    if mode == 1
+        [R, T] = scan_matcher(range_1, range_2);
+    elseif mode == 2
+        [R, T] = csm_scan_matcher(range_1, range_2);
+    elseif mode == 3
+        [init_x, init_y, init_theta] = ...
+            odometry_difference(pose_x(i), pose_y(i), pose_theta(i), pose_x(j), pose_y(j), pose_theta(j));
+
+        [R, T, covariance(:,p)] = ...
+            fast_csm_scan_matcher(robot.laser(i).measurement_time, range_1, ...
+            robot.laser(j).measurement_time, range_2, [init_x, init_y, init_theta]);
+    end
     
     Trans_local(:, p) = T;
     Rot_local(:, :, p) = R;
