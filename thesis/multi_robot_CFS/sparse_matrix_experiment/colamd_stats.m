ed6081d6eadf21d51fbc45b58446e939b7d6197b
nrkumar93 <nrkumar93@gmail.com>
2017-03-29 20:54:55 -0400

    row ordering also

--- thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_stats.m
+++ thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_stats.m
@@ -10,6 +10,7 @@ for i = 1:nvarargs
     dataset = [dataset unit_mat];
 end
 
+nnz_unordered = zeros(1,size(combinations,1));
 nnz_reorder = zeros(1,size(combinations,1));
 nnz_fused_order = zeros(1,size(combinations,1));
 nnz_myorder = zeros(1,size(combinations,1));
@@ -28,7 +29,7 @@ for i = 1:size(combinations,1)
         big_matrix_parts_ordered = blkdiag(dataset(combinations(i,j)).A(:,p1), dataset(combinations(i,j+1)).A(:,p2));
     end
     
-%     Doing my ordering only for sample_count 2.
+%     Doing my ordering only for sample_count 2 for columns.
     if size(dataset(combinations(i,1)).A, 2) > size(dataset(combinations(i,2)).A, 2)
         min_vec = size(dataset(combinations(i,1)).A, 2) + 1:size(big_matrix_unordered,2);
         max_vec = 1:size(dataset(combinations(i,1)).A, 2);
@@ -46,8 +47,30 @@ for i = 1:size(combinations,1)
         myorder((j+1)*(gap+1)+1:end) = (j+1)*(gap+1)+1:length(myorder);
     end
     
-    big_matrix_myorder = big_matrix_parts_ordered(:,myorder);
+%     Doing my ordering only for sample_count = 2 for rows.
+    if size(dataset(combinations(i,1)).A, 1) > size(dataset(combinations(i,2)).A, 1)
+        min_vec_row = size(dataset(combinations(i,1)).A, 1) + 1:size(big_matrix_unordered,1);
+        max_vec_row = 1:size(dataset(combinations(i,1)).A, 1);
+    else
+        min_vec_row = 1:size(dataset(combinations(i,1)).A, 1);
+        max_vec_row = size(dataset(combinations(i,1)).A, 1) + 1:size(big_matrix_unordered,1);
+    end
+    gap_row = floor(length(max_vec_row)/length(min_vec_row));
     
+    myorder_row = zeros(1,size(big_matrix_unordered,1));
+    for j = 0:length(min_vec_row)-1
+        myorder_row((j*gap_row)+(j+1): (j+1)*(gap_row+1)) = [max_vec_row((j*gap_row)+1:(j+1)*gap_row) min_vec_row(j+1)];
+    end
+    if (j+1)*(gap_row+1)+1 < size(big_matrix_unordered,1)
+        myorder_row((j+1)*(gap_row+1)+1:end) = (j+1)*(gap_row+1)+1:length(myorder_row);
+    end
+    
+    
+    big_matrix_myorder = big_matrix_parts_ordered(:,myorder);
+    big_matrix_myorder = big_matrix_myorder(myorder_row,:);
+    
+    R_unordered = qr(big_matrix_unordered);
+    nnz_unordered(i) = nnz(R_unordered);
     R_reorder = qr(big_matrix_whole_ordered);
     nnz_reorder(i) = nnz(R_reorder);
     R_fused_order = qr(big_matrix_parts_ordered);
@@ -55,19 +78,21 @@ for i = 1:size(combinations,1)
     R_myorder = qr(big_matrix_myorder);
     nnz_myorder(i) = nnz(R_myorder);
     
-    big_matrix_whole_ordered = [];
-    R_reorder = [];
-    big_matrix_parts_ordered = [];
-    R_fused_order = [];
-    big_matrix_myorder = [];
-    R_myorder = [];
+%     big_matrix_whole_ordered = [];
+%     R_reorder = [];
+%     big_matrix_parts_ordered = [];
+%     R_fused_order = [];
+%     big_matrix_myorder = [];
+%     R_myorder = [];
     
 end
 
 figure; hold on;
+plot(nnz_unordered);
 plot(nnz_reorder);
 plot(nnz_fused_order)
 plot(nnz_myorder);
+nnz_unordered;
 
 % figure; 
 % subplot(1,3,1);
