a39288380b757438c6a8b527f5401957ca1b08c0
nrkumar93 <nrkumar93@gmail.com>
2017-03-31 00:00:23 -0400

    lmap and sync additino

--- thesis/multi_robot_CFS/dataset_parser/raw_data_parser.m
+++ thesis/multi_robot_CFS/dataset_parser/raw_data_parser.m
@@ -1,15 +1,33 @@
 function dataset = raw_data_parser(data_stream)
 
-dataset = struct('odom', {}, 'laser', {}, 'fiducial', {});
+dataset = struct('odom', {}, 'laser', {}, 'fiducial', {}, 'lmap', {}, 'sync', {});
 
 laser_i = 1;
 odom_i = 1;
 fiducial_i = 1;
+lmap_i = 1;
+sync_i = 1;
 
 while ~feof(data_stream)
     unit_measurement = strsplit(fgets(data_stream));
     if strcmp(unit_measurement(4),'position') 
         dataset(1).odom(odom_i) = odom_parser(unit_measurement);
+        if odom_i - laser_i == 1
+            dataset(1).laser(laser_i) = laser_parser(false);
+            laser_i = laser_i + 1;
+        end
+        if odom_i - fiducial_i == 1
+            dataset(1).fiducial(fiducial_i) = fiducial_parser(false);
+            fiducial_i = fiducial_i + 1;
+        end
+        if odom_i - lmap_i == 1
+            dataset(1).lmap(lmap_i) = lmap_parser(false);
+            lmap_i = lmap_i + 1;        
+        end
+        if odom_i - sync_i == 1
+            dataset(1).sync(sync_i) = struct('sync', []);
+            sync_i = sync_i + 1;        
+        end        
         odom_i = odom_i + 1;
         continue;
     elseif strcmp(unit_measurement(4),'laser')
@@ -20,12 +38,13 @@ while ~feof(data_stream)
         dataset(1).fiducial(fiducial_i) = fiducial_parser(unit_measurement);
         fiducial_i = fiducial_i + 1;
         continue;
-    end
-    if odom_i - laser_i == 1
-        dataset(1).laser(laser_i) = laser_parser(false);
-        laser_i = laser_i + 1;
-    elseif odom_i - fiducial_i == 1
-        dataset(1).fiducial(fiducial_i) = fiducial_parser(false);
-        fiducial_i = fiducial_i + 1;
+    elseif strcmp(unit_measurement(4), 'lmap')
+        dataset(1).lmap(lmap_i) = lmap_parser(unit_measurement);
+        lmap_i = lmap_i + 1;
+        continue;
+    elseif strcmp(unit_measurement(4), 'sync')
+        dataset(1).sync(sync_i) = struct('sync', str2double(unit_measurement(6)));
+        sync_i = sync_i + 1;
+        continue;
     end
 end
