549e6d995bdbc8bc5b59db17975ce27a26ecfc4b
nrkumar93 <nrkumar93@gmail.com>
2017-04-03 19:33:57 -0400

    pushing everything... leaving BNR :(

--- thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_graph_combining.m
+++ thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_graph_combining.m
@@ -1,4 +1,4 @@
-function [] = colamd_chop_verification(episodes_per_problem, matrix_id)
+function [] = colamd_graph_combining(episodes_per_problem, matrix_id)
 
 
 sizes = zeros(1, length(matrix_id) * episodes_per_problem);
@@ -11,33 +11,43 @@ for sz = 1:length(matrix_id)
     problem = UFget(matrix_id(sz));
 
     input_matrix = problem.A;
+    if size(input_matrix, 1) < size(input_matrix, 2)
+         continue;
+    elseif size(input_matrix, 1) == size(input_matrix, 2)
+        input_matrix = input_matrix(:, 1:round(size(input_matrix, 2)/3));
+    end
     input_matrix_size = size(input_matrix,2);
-    input_matrix_order = colamd(input_matrix);
-    R = qr(input_matrix);
+    
+    
+%     input_matrix_order = colamd(input_matrix);
+%     R = qr(input_matrix);
 
     for i = 1:episodes_per_problem
-        sample_init_size = randi([round(input_matrix_size/20) round(input_matrix_size/10)], 1);
-        random_pick_vars = randsample(input_matrix_size, sample_init_size)';
+        sample_init_size_A = randi([round(input_matrix_size/20) round(input_matrix_size/10)], 1);
+        sample_init_size_B = randi([round(input_matrix_size/20) round(input_matrix_size/10)], 1);
+        random_pick_vars_A = randsample(input_matrix_size, sample_init_size_A)';
+        random_pick_vars_B = randsample(input_matrix_size, sample_init_size_B)';
         
-        total_affected_variables = random_pick_vars;
+        total_affected_variables_A = random_pick_vars_A;
+        total_affected_variables_B = random_pick_vars_B;
+
         k = 1;
-        
-        while k <= length(total_affected_variables)
-            row_considered_binary = zeros(1, input_matrix_size);
-            row_considered = R(total_affected_variables(k),:);
-            row_considered_binary(row_considered ~= 0) = 1;
-            assert(~any(total_affected_variables > input_matrix_size));
-            for l = total_affected_variables(k)+1:input_matrix_size
-                if row_considered_binary(l) > 0 && ~any(total_affected_variables == l)
-                    total_affected_variables = [total_affected_variables l];
+        while k <= length(total_affected_variables_A)
+            row_considered_binary_A = zeros(1, input_matrix_size);
+            row_considered = R(total_affected_variables_A(k),:);
+            row_considered_binary_A(row_considered ~= 0) = 1;
+            assert(~any(total_affected_variables_A > input_matrix_size));
+            for l = total_affected_variables_A(k)+1:input_matrix_size
+                if row_considered_binary_A(l) > 0 && ~any(total_affected_variables_A == l)
+                    total_affected_variables_A = [total_affected_variables_A l];
                 end
             end
             k = k + 1;
         end
         
-        sample_final_size = length(total_affected_variables);
+        sample_final_size = length(total_affected_variables_A);
 %         sample_matrix_col_indices = randsample(input_matrix_size, sample_matrix_size);
-        sample_matrix_col_indices = total_affected_variables;
+        sample_matrix_col_indices = total_affected_variables_A;
         sample_matrix = input_matrix(:, sample_matrix_col_indices);
         sample_matrix_colamd_order = colamd(sample_matrix);
 
