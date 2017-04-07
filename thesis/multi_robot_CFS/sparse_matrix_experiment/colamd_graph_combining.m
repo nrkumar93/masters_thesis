0b45ce7401dc3a5664dfef0634601497a9655d09
nrkumar93 <nrkumar93@gmail.com>
2017-04-06 23:57:30 -0400

    untidy work. but added some important constraints

--- thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_graph_combining.m
+++ thesis/multi_robot_CFS/sparse_matrix_experiment/colamd_graph_combining.m
@@ -11,18 +11,29 @@ for sz = 1:length(matrix_id)
     problem = UFget(matrix_id(sz));
 
     input_matrix = problem.A;
-    if size(input_matrix, 1) < size(input_matrix, 2)
-         continue;
-    elseif size(input_matrix, 1) == size(input_matrix, 2)
-        input_matrix = input_matrix(:, 1:round(size(input_matrix, 2)/3));
+    if size(input_matrix, 1) < 2 * size(input_matrix, 2)
+        input_matrix = input_matrix(:, 1:round(size(input_matrix, 2)/2.5)); % 2.5 is some number greater than 2.
     end
     input_matrix_size = size(input_matrix,2);
     
+    input_matrix_A = input_matrix(1:floor(size(input_matrix,1)/2), :);
+    input_matrix_B = input_matrix(ceil(size(input_matrix,1)/2):size(input_matrix,1), :);
+    input_matrix_size_A = size(input_matrix_A,2); % TODO: Delete these vars
+    input_matrix_size_B = size(input_matrix_B,2);
+    
+    assert(input_matrix_size == input_matrix_size_A);
+    assert(input_matrix_size == input_matrix_size_B); 
     
 %     input_matrix_order = colamd(input_matrix);
 %     R = qr(input_matrix);
 
-    for i = 1:episodes_per_problem
+    input_matrix_order_A = colamd(input_matrix_A);
+    input_matrix_order_B = colamd(input_matrix_B);
+    R_A = qr(input_matrix_A);
+    R_B = qr(input_matrix_B);
+
+    i = 1;
+    while i <= episodes_per_problem
         sample_init_size_A = randi([round(input_matrix_size/20) round(input_matrix_size/10)], 1);
         sample_init_size_B = randi([round(input_matrix_size/20) round(input_matrix_size/10)], 1);
         random_pick_vars_A = randsample(input_matrix_size, sample_init_size_A)';
@@ -33,56 +44,74 @@ for sz = 1:length(matrix_id)
 
         k = 1;
         while k <= length(total_affected_variables_A)
-            row_considered_binary_A = zeros(1, input_matrix_size);
-            row_considered = R(total_affected_variables_A(k),:);
-            row_considered_binary_A(row_considered ~= 0) = 1;
+            row_considered_binary = zeros(1, input_matrix_size);
+            row_considered = R_A(total_affected_variables_A(k),:);
+            row_considered_binary(row_considered ~= 0) = 1;
             assert(~any(total_affected_variables_A > input_matrix_size));
             for l = total_affected_variables_A(k)+1:input_matrix_size
-                if row_considered_binary_A(l) > 0 && ~any(total_affected_variables_A == l)
+                if row_considered_binary(l) > 0 && ~any(total_affected_variables_A == l)
                     total_affected_variables_A = [total_affected_variables_A l];
                 end
             end
             k = k + 1;
         end
         
-        sample_final_size = length(total_affected_variables_A);
+        k = 1;
+        while k <= length(total_affected_variables_B)
+            row_considered_binary = zeros(1, input_matrix_size);
+            row_considered = R_B(total_affected_variables_B(k),:);
+            row_considered_binary(row_considered ~= 0) = 1;
+            assert(~any(total_affected_variables_B > input_matrix_size));
+            for l = total_affected_variables_B(k)+1:input_matrix_size
+                if row_considered_binary(l) > 0 && ~any(total_affected_variables_B == l)
+                    total_affected_variables_B = [total_affected_variables_B l];
+                end
+            end
+            k = k + 1;
+        end 
+        
+        if isempty(intersect(total_affected_variables_A, total_affected_variables_B))
+            continue;
+        elseif isequal(total_affected_variables_A, total_affected_variables_B)
+            continue;
+        end
+        
+        sample_final_size = length(union(total_affected_variables_A, total_affected_variables_B));
 %         sample_matrix_col_indices = randsample(input_matrix_size, sample_matrix_size);
-        sample_matrix_col_indices = total_affected_variables_A;
-        sample_matrix = input_matrix(:, sample_matrix_col_indices);
+%         sample_matrix_col_indices = total_affected_variables_A;
+        pure_col_indices_A = setdiff(total_affected_variables_A, intersect(total_affected_variables_A, total_affected_variables_B));
+        pure_col_indices_B = setdiff(total_affected_variables_B, intersect(total_affected_variables_A, total_affected_variables_B));
+        common_col_indices = intersect(total_affected_variables_A, total_affected_variables_B);
+        sample_matrix = blkdiag(input_matrix_A(:,pure_col_indices_A), input_matrix_B(:,pure_col_indices_B));
+        for j = intersect(total_affected_variables_A, total_affected_variables_B)
+            sample_matrix = [sample_matrix [input_matrix_A(:,j); input_matrix_B(:,j)]];
+        end
+        
+%         sample_matrix = input_matrix(:, sample_matrix_col_indices);
         sample_matrix_colamd_order = colamd(sample_matrix);
-
-        pos_in_input_matrix = zeros(1, sample_final_size);
-        for j = 1:sample_final_size
-    %         pos_in_input_matrix(j) = input_matrix_order(sample_matrix_col_indices(j));
-            pos_in_input_matrix(j) = find(input_matrix_order == sample_matrix_col_indices(j));
-        end
-
-%         [~, mlab_order] = sort(pos_in_input_matrix);
-        sample_matrix_myorder = zeros(1, sample_final_size);
-%         sample_matrix_myordera = zeros(1, sample_matrix_size);
-
-        for j = 1:sample_final_size
-            [~,ind] = min(pos_in_input_matrix);
-            pos_in_input_matrix(ind) = inf;
-            sample_matrix_myorder(j) = ind;
-    %         sample_matrix_myordera(j) = find(sample_matrix_col_indices(ind) == sample_matrix_col_indices);
-        end
-
-    %     assert(isequal(sample_matrix_myordera, sample_matrix_myorder));
+        
+        myorder_A = relative_ordering(input_matrix_order_A, pure_col_indices_A);
+        myorder_B = relative_ordering(input_matrix_order_B, pure_col_indices_B);
+        myorder_B = myorder_B + length(myorder_A);
+        myorder_common = relative_ordering(input_matrix_order_A, common_col_indices);
+        myorder_common = myorder_common + length(myorder_B) + length(myorder_A);
+        myorder = [myorder_A myorder_B myorder_common];
 
 
-        nnz_raw(((sz-1) * episodes_per_problem) + i) = nnz(qr(sample_matrix))/numel(sample_matrix);
+        nnz_raw(((sz-1) * episodes_per_problem) + i) = nnz(qr(sample_matrix(:, fliplr(sample_matrix_colamd_order))))/numel(sample_matrix);
         nnz_colamd(((sz-1) * episodes_per_problem) + i) = nnz(qr(sample_matrix(:, sample_matrix_colamd_order)))/numel(sample_matrix);
-        nnz_myorder(((sz-1) * episodes_per_problem) + i) = nnz(qr(sample_matrix(:, sample_matrix_myorder)))/numel(sample_matrix);
+        nnz_myorder(((sz-1) * episodes_per_problem) + i) = nnz(qr(sample_matrix(:, myorder)))/numel(sample_matrix);
 
-        sizes(((sz-1) * episodes_per_problem) + i) = sample_final_size;
-        dist(((sz-1) * episodes_per_problem) + i) = pdist2(sample_matrix_colamd_order, sample_matrix_myorder, 'hamming');
+%         sizes(((sz-1) * episodes_per_problem) + i) = sample_final_size;
+%         dist(((sz-1) * episodes_per_problem) + i) = pdist2(sample_matrix_colamd_order, sample_matrix_myorder, 'hamming');
+        
+        i = i + 1;
     end
 end
 
-figure;
-hold on;
-plot(sizes, dist, 'r*');
+% figure;
+% hold on;
+% plot(sizes, dist, 'r*');
 
 figure;
 hold on;
@@ -98,5 +127,39 @@ stem(nnz_myorder, 'filled', 'Marker', 'd', 'LineWidth', 0.75);
 
 hold off;
 
+end
+
+function myorder = relative_ordering(colamd_order, query_indices)
+        query_size = length(query_indices);
+        pos_in_input_matrix = zeros(1, query_size);
+        for j = 1:query_size
+    %         pos_in_input_matrix(j) = input_matrix_order(sample_matrix_col_indices(j));
+%     Logs the index in which the value query_indices(j) is present in
+%     colamd_order array in pos_in_input_matrix(j)
+            pos_in_input_matrix(j) = find(colamd_order == query_indices(j));
+        end
+
+%         [~, mlab_order] = sort(pos_in_input_matrix);
+        myorder = zeros(1, query_size);
+%         sample_matrix_myordera = zeros(1, sample_matrix_size);
+
+        pos_in_input_matrix_copy = pos_in_input_matrix;
+
+        for j = 1:query_size
+            [~,ind] = min(pos_in_input_matrix);
+            pos_in_input_matrix(ind) = inf;
+            myorder(j) = ind;
+    %         sample_matrix_myordera(j) = find(sample_matrix_col_indices(ind) == sample_matrix_col_indices);
+        end
+        
+        figure;
+        hold on;
+        plot(pos_in_input_matrix_copy, 'b');
+        plot(myorder, 'r');
+        clf;
+
+    %     assert(isequal(sample_matrix_myordera, sample_matrix_myorder));
+
+end
 
 
