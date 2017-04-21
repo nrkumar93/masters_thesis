7a7e282d0c49551e4f9ecc2d43872d417aee1e2b
nrkumar93 <nrkumar93@gmail.com>
2017-04-20 23:21:48 -0700

    top level changes

--- thesis/multi_robot_CFS/modular_iSAM/top_level.m
+++ thesis/multi_robot_CFS/modular_iSAM/top_level.m
@@ -2,6 +2,8 @@ clear;
 
 import gtsam.*
 
+multi_robot_mode = true;
+
 robot_activation_mask = [0 1 1 0];
 robot_data_end = [nan 13000 inf nan];
 
@@ -11,6 +13,29 @@ params = load_params(robot_activation_mask);
 
 setup_covariances(robot_activation_mask, data, params);
 
+% The constraint digit
+% 1st digit: cooperative mapping
+% 2nd digit: odom
+% 3rd digit: velocity model
+% 4th digit: lmap
+% 5th digit: scan matching
+% 6th digit: fiducials
+% 7th digit: loop closure
 
+constraints_code = zeros(length(robot_activation_mask), 7);
+for i = 1:length(robot_activation_mask)
+    constraints_code(i) = [multi_robot_mode, ...
+                       params(i).odometry_flag, ...
+                       params(i).velocity_model_flag, ...
+                       params(i).lmap_flag, ...
+                       params(i).scan_matching_flag, ...
+                       params(i).fiducial_flag, ...
+                       params(i).loop_closure_flag];
+end
 
+constraints_code = bitand(bitand(bitand(constraints_code(1,:), ...
+                                  constraints_code(2,:)), ...
+                                  constraints_code(3,:)), ...
+                                  constraints_code(4,:));
 
+constraints_code = bi2de(constraints_code);
