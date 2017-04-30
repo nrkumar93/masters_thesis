5352acab29562e9695f63e9a3bfa986d96ec10b1
nrkumar93 <nrkumar93@gmail.com>
2017-04-30 01:19:40 -0400

    finishing up the optimizer. some edge case handling needed for robot encounter

--- thesis/multi_robot_CFS/modular_iSAM/optimize.m
+++ thesis/multi_robot_CFS/modular_iSAM/optimize.m
@@ -1,20 +1,68 @@
-function [] = optimize(mode, graph, robot_id, var_2)
+function result = optimize(mode, robot_id, robot_activation_mask, var_2, graph, result)
 
-global multi_isam;
-global isam initial;
+import gtsam.*
+
+global multi_robot robot_interaction_adjacency;
+global isam initial current_factor_indices key_offset;
 global init_x init_y init_theta;
 
-if isequal(mode, 'batch')
-    batchOptimizer = LevenbergMarquardtOptimizer(graph, initial);
-    initial = batchOptimizer.optimize();    
+persistent batch_initialization visit_counter;
+if isempty(batch_initialization)
+    batch_initialization = zeros(length(initial),1);
+end
+if isempty(find(visit_counter == robot_id,1))
+    visit_counter = [visit_counter robot_id];
+    visit_counter = sort(visit_counter);
 end
 
-isam(robot_id).update(graph, initial);
-result = isam(robot_id).calculateEstimate();
-init_x = result.at(var_2).x;
-init_y = result.at(var_2).y;
-init_theta = result.at(var_2).theta;
+if isequal(mode, 'batch')
+    batchOptimizer = LevenbergMarquardtOptimizer(graph, initial(robot_id));
+    initial(robot_id) = batchOptimizer.optimize();
+    batch_initialization(robot_id) = 1;
+end
 
-graph = NonlinearFactorGraph;
-initial = Values;
+isam(robot_id).update(graph, initial(robot_id));
+result(robot_id) = isam(robot_id).calculateEstimate();
+
+var_2 = robot_id * key_offset(robot_id) + var_2;                
+init_x(robot_id) = result(robot_id).at(var_2).x;
+init_y(robot_id) = result(robot_id).at(var_2).y;
+init_theta(robot_id) = result(robot_id).at(var_2).theta;
+
+for i = current_factor_indices{robot_id}'
+    % The first operand of the && operation in both if and elseif looks
+    % redundant because the variable is added to "robot_id" index of
+    % current_factor_indices only when an initial value is set for the same
+    % variable in "robot_id" index of multi_robot.initial. So all the
+    % variables in current_factor_indices(robot_id) should by definition
+    % exist in multi_robot.initial(robot_id).
+    %
+    % ASSUMPTION: key_offset for all the robots are SAME.
+    if multi_robot.initial(robot_id).exists(i) && result(robot_id).exists(i)
+        multi_robot.initial(robot_id).update(i,result(robot_id).at(i));
+    elseif multi_robot.initial(robot_id).exists(i) && result(floor(i/key_offset(robot_id))).exists(i)
+        multi_robot.initial(robot_id).update(i,result(floor(i/key_offset(robot_id))).at(i));
+    end
+end
+
+if isequal(find(robot_activation_mask == 1), visit_counter) && isequal(batch_initialization', robot_activation_mask)
+    for i = 1:size(robot_interaction_adjacency,1)
+        for j = 1:i-1
+            if i ~= j && robot_interaction_adjacency(i,j) > 0
+                multi_robot.isam.update(multi_robot.graph(i), multi_robot.initial(i));
+                multi_robot.isam.update(multi_robot.graph(j), multi_robot.initial(j));
+                multi_robot.graph(i) = NonlinearFactorGraph;
+                multi_robot.graph(j) = NonlinearFactorGraph;
+                multi_robot.initial(i) = Values;
+                multi_robot.initial(j) = Values;
+                robot_interaction_adjacency(i,j) = 0;
+                robot_interaction_adjacency(j,i) = 0;
+                current_factor_indices{i} = [];
+                current_factor_indices{j} = [];
+            end
+        end
+    end
+    visit_counter = [];
+end
+initial(robot_id) = Values;
 
--- thesis/multi_robot_CFS/modular_iSAM/top_level.m
+++ thesis/multi_robot_CFS/modular_iSAM/top_level.m
@@ -1,4 +1,6 @@
 clear;
+clear global
+clear optimize scan_match_pattern_selector
 
 import gtsam.*
 
@@ -13,7 +15,7 @@ params = load_params(robot_activation_mask);
 
 setup_covariances(robot_activation_mask, data, params);
 
-setup_globals();
+setup_globals(robot_activation_mask, data, params);
 
 % The constraint digit
 % 1st digit: cooperative mapping
@@ -26,13 +28,17 @@ setup_globals();
 
 constraints_code = zeros(length(robot_activation_mask), 7);
 for i = 1:length(robot_activation_mask)
-    constraints_code(i) = [multi_robot_mode, ...
-                       params(i).odometry_flag, ...
-                       params(i).velocity_model_flag, ...
-                       params(i).lmap_flag, ...
-                       params(i).scan_matching_flag, ...
-                       params(i).fiducial_flag, ...
-                       params(i).loop_closure_flag];
+    if robot_activation_mask(i) ~= 0
+        constraints_code(i,:) = [multi_robot_mode, ...
+                           params(i).odometry_flag, ...
+                           params(i).velocity_model_flag, ...
+                           params(i).lmap_flag, ...
+                           params(i).scan_matching_flag, ...
+                           params(i).fiducial_flag, ...
+                           params(i).loop_closure_flag];
+    else
+        constraints_code(i,:) = ones(1,7);
+    end
 end
 
 constraints_code = bitand(bitand(bitand(constraints_code(1,:), ...
@@ -41,3 +47,5 @@ constraints_code = bitand(bitand(bitand(constraints_code(1,:), ...
                                   constraints_code(4,:));
 
 constraints_code = bi2de(constraints_code);
+
+multi_sam(robot_activation_mask, constraints_code, data, params);
